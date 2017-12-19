a3cbe4d73432d42c5552880f000e6730eb7fbf3d
lvmingliang <lvminlgiang_glut@163.com>
2017-12-19 13:42:02 +0800

    Configuration
    熟悉配置：

--- src/main/java/com/lv/girl/HelloController.java
+++ src/main/java/com/lv/girl/HelloController.java
@@ -3,10 +3,16 @@ package com.lv.girl;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @Author lvmingliang_glut@163.com
+ * @Date 2017/12/19 13:07
+ * @Description
+ **/
+
 @RestController
 public class HelloController {
-    @RequestMapping(value = "/hello")
-    public String say() {
-        return "Hello Spring　Boot";
+    @RequestMapping
+    public String say(){
+        return "hello SpringBoot!";
     }
 }
