a9a447db57967b9b120ee8c37c4bb226984768e7
lvmingliang <lvminlgiang_glut@163.com>
2017-12-19 13:51:55 +0800

    属性配置
    yml文件的使用

--- src/main/java/com/lv/girl/HelloController.java
+++ src/main/java/com/lv/girl/HelloController.java
@@ -1,5 +1,6 @@
 package com.lv.girl;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    @Value("${name}")
+    private String name;
+    @Value("${age}")
+    private Integer age;
+    @Value("${cupSize}")
+    private String cupSize;
+
+    @Value("${content}")
+    private String content;
     @RequestMapping
     public String say(){
-        return "hello SpringBoot!";
+        return "my'name is "+name +" age is "+age +" cupZize:" +cupSize +"--"+content;
     }
 }
